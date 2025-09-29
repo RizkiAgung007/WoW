7b9349aeb65e07dfc034cba61a4202bbacd50719
RizkiAgung007 <client689k@gmail.com>
2025-09-29 14:48:39 +0700

    update payload

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -5,19 +5,25 @@ dotenv.config();
 function verifyToken(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Token tidak ditemukan atau format salah" });
+    return res
+      .status(401)
+      .json({ message: "Token tidak ditemukan atau format salah" });
   }
 
   const token = authHeader.split(" ")[1];
+
   if (!token) {
     return res.status(401).json({ message: "Token tidak ditemukan." });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_KEY);
-    req.userId = decoded.id;
+    req.userId = decoded.user.id;
     next();
   } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Token sudah kadaluarsa." });
+    }
     return res.status(403).json({ message: "Token tidak valid." });
   }
 }
